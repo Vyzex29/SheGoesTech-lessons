e41631a325820cf41bf92dcc8e0e248c4f3d69eb
MontaTroksa <100307165+MontaTroksa@users.noreply.github.com>
2022-02-23 21:03:13 +0200

    Update Helloworld.cs

--- Helloworld.cs
+++ Helloworld.cs
@@ -7,6 +7,7 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Monta");
             Console.WriteLine("Olga");
             Console.WriteLine("From Github - Valerijs");
 
