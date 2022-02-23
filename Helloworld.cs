65ca0f5ead86da4c5af9967c8ac880b4b46ec9ae
Zane-Silina <100307105+Zane-Silina@users.noreply.github.com>
2022-02-23 21:02:42 +0200

    Update Helloworld.cs

--- Helloworld.cs
+++ Helloworld.cs
@@ -7,8 +7,10 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Zane");
             Console.WriteLine("Olga");
             Console.WriteLine("From Github - Valerijs");
+          
 
         }
     }
