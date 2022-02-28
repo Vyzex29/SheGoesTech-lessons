8e7b64d74618821061d759e086ce22aaa602a73c
Greta Kaminskaite <greta.kam@gmail.com>
2022-02-28 20:25:14 +0200

    Greta K. var 5

--- SGTCalculator/SGTCalculator.cs
+++ SGTCalculator/SGTCalculator.cs
@@ -21,20 +21,20 @@
             Console.WriteLine(result);
 
 
+
             // Next Variants should goc below this line
-            // Variant 20. Renate V.
-            // 5a - 3b + c^7 - x | a=6, b=4, c=2, x=1.15
 
-            varA = 6;
-            varB = 4;
-            varC = 2;
-            varX = 1.15;
+            // Variant 5. Greta K.
+            // 9a + sqrt(2b) - c | a = 7, b = 26, c = 16
 
-            result2 = 5 * varA – 3 * varB + Math.Pow(varC, 7) – varX;
-            Console.WriteLine("Variant 20. Renate V." /n);
-            Console.WriteLine(result2);
+            varA = 7;
+            varB = 26;
+            varC = 16;
 
+            result = 9 * varA + Math.Sqrt(2 * varB) - varC;
+            Console.WriteLine($"Variant 5. Greta K. \n{result}");
 
+            
             // Variant 21. Valerijs D.
             // 9a + sqrt(2b) -c | a=7, b=26, c=16 
 
@@ -45,6 +45,9 @@
             result = 9 * varA + Math.Sqrt(2 * varB) - varC;
             Console.WriteLine("Variant 21. Valerijs D.");
             Console.WriteLine(result);
+
+
+            
         }
     }
 }
