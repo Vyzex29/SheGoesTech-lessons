0d99ef3f0093964d459840067868789a9ed1c1aa
Ieva Petrusenkova <ieva.pilenge@gmail.com>
2022-02-28 20:28:23 +0200

    IevaP11

--- SGTCalculator/SGTCalculator.cs
+++ SGTCalculator/SGTCalculator.cs
@@ -22,6 +22,17 @@
 
 
             // Next Variants should goc below this line
+            // Variant 11. Ieva P.
+            // 7a + 12.5b + x +5y | a=5, b=1, x=7.01, y=3
+
+            varA = 5;
+            varB = 1;
+            varX = 7.01;
+            varY = 3;
+
+            result = 7 * varA + 12.5 * varB + varX + 5 * varY;
+            Console.WriteLine("Variant 11.Ieva P.");
+            Console.Write(result);
         }
     }
 }
