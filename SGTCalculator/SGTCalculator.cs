c8086347425058e123df2285d39dfefa69acca5d
DoraLiv <dora.livkisa@gmail.com>
2022-02-28 20:35:03 +0200

    Added var 7

--- SGTCalculator/SGTCalculator.cs
+++ SGTCalculator/SGTCalculator.cs
@@ -32,6 +32,17 @@
             result = 9 * varA + Math.Sqrt(2 * varB) - varC;
             Console.WriteLine("Variant 21. Valerijs D.");
             Console.WriteLine(result);
+
+            // Next Variants should goc below this line
+            // Variant 7. Dora L.
+            // 7a + 12.5b + x +5y | a=5, b=1, x=7.01, y=3
+            varA = 5;
+            varB = 1;
+            varX = 7.01;
+            varY = 3;
+            result = 7 * varA + 12.5 * varB + varX + 5 * varY;
+            Console.WriteLine("Variant 7. Dora L.");
+            Console.WriteLine(result);
         }
     }
 }
