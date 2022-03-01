90ba40c414fd8e864e071497886a63e8ed0ddeb1
VioletaJan <viol.viso@gmail.com>
2022-03-01 13:16:41 +0100

    not working

--- SGTCalculator/SGTCalculator.cs
+++ SGTCalculator/SGTCalculator.cs
@@ -45,20 +45,21 @@
             result = 9 * varA + Math.Sqrt(2 * varB) - varC;
             Console.WriteLine("Variant 21. Valerijs D.");
             Console.WriteLine(result);
+
+            //Variant 3. Violeta 
+            //7a + 12.5b + x +5y | a=5, b=1, x=7.01, y=3 
+
+            varA = 5;
+            varB = 1;
+            varX = 7.01;
+            varY = 3;
+
+            result = 7 * varA + 12.5 * varB + varX + 5 * varY;
+            Console.WriteLine("Variant 3. Violeta");
+            Console.WriteLine(result);
         }
     }
 }
 
-//Variant 3. Violeta 
-//7a + 12.5b + x +5y | a=5, b=1, x=7.01, y=3 
-
-varA = 5;
-varB = 1;
-varX = 7.01;
-varY = 3;
-
-result = 7 * varA + 12.5 * varB + Math.Pow(varX) + 5 * varY;
-Console.WriteLine("Variant 3. Violeta");
-Console.WriteLine(result); 
 
 
