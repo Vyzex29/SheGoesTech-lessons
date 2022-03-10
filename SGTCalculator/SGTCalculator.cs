abbc12b50ec194df08ccd2e2bccef7c11e3de40d
Indre Surviliene <indre.bitinaite@gmail.com>
2022-03-10 17:43:44 +0200

    Day8_Homework_IndreS

--- SGTCalculator/SGTCalculator.cs
+++ SGTCalculator/SGTCalculator.cs
@@ -45,6 +45,17 @@
             result = 9 * varA + Math.Sqrt(2 * varB) - varC;
             Console.WriteLine("Variant 21. Valerijs D.");
             Console.WriteLine(result);
+
+            // Variant 17. Indre
+            // 9a + sqrt(2b) - c | a = 7, b = 26, c = 16
+
+            varA = 7;
+            varB = 26;
+            varC = 16;
+
+            result = 9 * varA + Math.Sqrt(2 * varB) - varC;
+            Console.WriteLine("Variant17. Indre");
+            Console.WriteLine(result);
         }
     }
 }
