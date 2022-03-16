7d72455a27b27e88328de415c4838de7f43604d2
ValerijsDiks <valerijs.diks@lightsoft.lv>
2022-03-16 19:07:51 +0200

    Add columnSum

--- SGTCalculator/SGTCalculator.cs
+++ SGTCalculator/SGTCalculator.cs
@@ -4,41 +4,53 @@
     {
         static void Main(string[] args)
         {
-            double[,] array2d = new double[3,5];
+            double[,] array2d = new double[3,5]; // 2d array definition
+            double[] sumOfColumns = new double[array2d.GetLength(1)]; // definiton of 1d Array for sum Of columns using the GetLength of columns of 2darray
 
             var random = new Random();
-            for (int i = 0; i < array2d.GetLength(0); i++)
+            for (int i = 0; i < array2d.GetLength(0); i++)  // nested for loop for initializing the values inside the 2dArray
             {
                 for(int j = 0; j < array2d.GetLength(1); j++)
                 {
-                    array2d[i,j] = Math.Round(random.NextDouble() * 1000, 2);
+                    double randomNumber = random.NextDouble();
+                    array2d[i,j] = Math.Round(randomNumber * 100, 2);
                 }
             }
 
             double sumOfRow = 0;
 
-            for (int i = 0; i < array2d.GetLength(0); i++)
+            for (int row = 0; row < array2d.GetLength(0); row++) //NESTED LOOP
             {
+                for (int column = 0; column < array2d.GetLength(1); column++)
+                {
+                    Console.Write($"|{array2d[row, column]}| ");
+                    sumOfRow += array2d[row, column];
+                }
+
                 for (int j = 0; j < array2d.GetLength(1); j++)
                 {
-                    Console.Write($"|{array2d[i, j]}| ");
-                    sumOfRow += array2d[i, j];
-                }
+                    sumOfColumns[j] += array2d[row, j];
+                } 
+
                 Console.Write($" = {sumOfRow}");
-                sumOfRow = 0;
                 Console.WriteLine();
+                sumOfRow = 0;
             }
 
-
+            foreach (var sum in sumOfColumns)
+            {
+                Console.Write($" {Math.Round(sum,2)} ");
+            }
 
             double sumOfElements = 0;
+
 
             foreach (double element in array2d)
             {
                 sumOfElements += element;
             }
 
-            Console.WriteLine($"Sum of elements of the array is {sumOfElements}");
+            Console.WriteLine($"\nSum of elements of the array is {Math.Round(sumOfElements,2)}");
         }
     }
 }
