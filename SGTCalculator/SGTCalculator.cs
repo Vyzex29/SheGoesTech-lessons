f5bc54b5c9c20c6efcbcbb01eeac4002d5dce901
ValerijsDiks <valerijs.diks@lightsoft.lv>
2022-03-22 19:25:47 +0200

    Zamn

--- SGTCalculator/SGTCalculator.cs
+++ SGTCalculator/SGTCalculator.cs
@@ -1,143 +1,109 @@
-﻿namespace SGTCalculator // Note: actual namespace depends on the project name.
+﻿using System.Text.RegularExpressions;
+
+namespace SGTCalculator
 {
     public class SGTCalculator
     {
         static void Main(string[] args)
         {
-            // Define variables
-            int varA, varB, varC;
-            double varX, varY, varZ;
-            double result;
-
-            // Variant 1. Valerijs D.
-            // a^3 + 5*b + sqrt(x)
-            // a = 7, b = 95, x = 179.62
-            varA = 7;
-            varB = 95;
-            varX = 179.62;
-
-            result = Math.Pow(varA, 3) + 5 * varB + Math.Sqrt(varX);
-            Console.WriteLine("Variant 1. Valerijs D.");
-            Console.WriteLine(result);
-          
-            // Next Variants should goc below this line
-
-            // Variant 4. Lorianda
-            // 5a - 3b + c^7 - x | a=6, b=4, c=2, x=1.15
-            varA = 6;
-            varB = 4;
-            varC = 2;
-            varX = 1.15;
-
-            result = 5 * varA - 3 * varB + Math.Pow(varC, 7) - varX;
-            Console.WriteLine("Variant 4. Lorianda.");
-            Console.WriteLine(result);
-            
-          
-            // Next Variants should goc below this line
-            
-            // Variant 5. Greta K.
-            // 9a + sqrt(2b) - c | a = 7, b = 26, c = 16
-
-            varA = 7;
-            varB = 26;
-            varC = 16;
-            result = 9 * varA + Math.Sqrt(2 * varB) - varC;
-            Console.WriteLine($"Variant 5. Greta K. \n{result}");
-
-            // Next Variants should goc below this line
-
-            // Variant 11. Ieva P.
-            // 7a + 12.5b + x +5y | a=5, b=1, x=7.01, y=3
-
-            varA = 5;
-            varB = 1;
-            varX = 7.01;
-            varY = 3;
-
-            result = 7 * varA + 12.5 * varB + varX + 5 * varY;
-            Console.WriteLine("Variant 11.Ieva P.");
-            Console.Write(result);
-         
-            // Next Variants should go below this line
-            
-            // Variant 16. Olga S.
-            // 5a - 3b + c ^ 7 - x
-            // a = 6, b = 4, c = 2, x = 1.15
-        
-            varA = 6;
-            varB = 4;
-            varC = 2;
-            varX = 1.15;
-
-            Console.WriteLine();
-            Console.WriteLine("Variant 16. Olga S.");
-            Console.WriteLine(result);
-                   
-            // Next Variants should go below this line
-            
-            // Variant 17. Indre
-            // 9a + sqrt(2b) - c | a = 7, b = 26, c = 16
-
-            varA = 7;
-            varB = 26;
-            varC = 16;
-
-            result = 9 * varA + Math.Sqrt(2 * varB) - varC;
-            Console.WriteLine("Variant17. Indre");
-            Console.WriteLine(result);
-         
-            // Next Variants should go below this line
-            
-            // Variant 21. Valerijs D.
-            // 9a + sqrt(2b) -c | a=7, b=26, c=16 
-
-            varA = 7;
-            varB = 26;
-            varC = 16;
-
-            result = 9 * varA + Math.Sqrt(2 * varB) - varC;
-            Console.WriteLine("Variant 21. Valerijs D.");
-            Console.WriteLine(result);
-            
-            // Next Variants should goc below this line
-            // Variant 9. Julija N.
-            // 9a + sqrt(2b) -c | a=7, b=26, c=16
-
-            varA = 7;
-            varB = 26;
-            varC = 16;
-
-            result = 9 * varA + Math.Sqrt(2 * varB) - varC;
-            Console.WriteLine("Variant 9. Julija N.");
-            Console.WriteLine(result);
-
-                   
-            // Next Variants should go below this line
-            
-            //22.Extra if wanted Zane
-            //2a + 3 * b - x ^ 2 | a = 7, b = 2, x = 1.55
-
-            varA = 7;
-            varB = 2;
-            varX = 1.55;
-            result = 2 * varA + 3 * varB - Math.Pow(varX, 2);
-
-            Console.WriteLine("Variant 22. Zane");
-            Console.WriteLine(result);
-          
-          //Variant 14. Irena 
-          // 2a + 3b - x ^ 2 | a = 7, b = 2, x = 1.55
-            varA = 7;
-            varB = 2;
-            varX = 1.55;
-            result = 2 * varA + 3 * varB -Math.Pow(varX, 2);
-            Console.WriteLine("Variant 14. Irena");
-            Console.WriteLine(result);
 
 
-            Console.ReadKey();
+            Console.WriteLine("=======================================");
 
+            /* A.*s -- Ananass
+              ^ StartsWith --> mathch StartsWithThisSentence
+              Contains only from alphanumeric a-z (lower case only)
+              [a-z]+ one or more times
+                \d == [0-9]
+            */
+
+            // validate this range: 1900 - 2100
+
+            string validInput = "2020";
+            string invalidInput = "199029";
+
+            // build a pattern to validate a year
+            var reg = new Regex("^\\d\\d\\d\\d$"); // either double slash or @
+
+            if (reg.IsMatch(validInput))
+            {
+                Console.WriteLine($"{validInput} is valid year");
+            }
+            else
+            {
+                Console.WriteLine($"{validInput} is not a valid year");
+            }
+
+            if (reg.IsMatch(invalidInput))
+            {
+                Console.WriteLine($"{invalidInput} is valid year");
+            }
+            else
+            {
+                Console.WriteLine($"{invalidInput} is not a valid year");
+            }
+
+            // but this allows 2999
+            var regImprovement = new Regex("^[1-2]{1}[019]{1}\\d\\d$");
+
+            Console.WriteLine("=======Enhanced validation rules=====");
+            if (regImprovement.IsMatch(validInput))
+            {
+                Console.WriteLine($"{validInput} is valid year");
+            }
+            else
+            {
+                Console.WriteLine($"{validInput} is not a valid year");
+            }
+
+            if (regImprovement.IsMatch(invalidInput))
+            {
+                Console.WriteLine($"{invalidInput} is valid year");
+            }
+            else
+            {
+                Console.WriteLine($"{invalidInput} is not a valid year");
+            }
+
+            var regImprovementNext = new Regex("^(19|20)\\d\\d$");
+            Console.WriteLine("=======Enhanced validation rules V2=====");
+            if (regImprovementNext.IsMatch(validInput))
+            {
+                Console.WriteLine($"{validInput} is valid year");
+            }
+            else
+            {
+                Console.WriteLine($"{validInput} is not a valid year");
+            }
+
+            if (regImprovementNext.IsMatch(invalidInput))
+            {
+                Console.WriteLine($"{invalidInput} is valid year");
+            }
+            else
+            {
+                Console.WriteLine($"{invalidInput} is not a valid year");
+            }
+
+            var regImprovementFinal = new Regex("^((19|20)\\d\\d|2100)$"); // grouping
+            Console.WriteLine("=======Final validation rules=====");
+            if (regImprovementFinal.IsMatch(validInput))
+            {
+                Console.WriteLine($"{validInput} is valid year");
+            }
+            else
+            {
+                Console.WriteLine($"{validInput} is not a valid year");
+            }
+
+            if (regImprovementFinal.IsMatch(invalidInput))
+            {
+                Console.WriteLine($"{invalidInput} is valid year");
+            }
+            else
+            {
+                Console.WriteLine($"{invalidInput} is not a valid year");
+            }
         }
     }
 }
