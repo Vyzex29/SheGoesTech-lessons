1ca03b29597246a3ccd8b8e0f16a76de3610e2e2
ValerijsDiks <valerijs.diks@lightsoft.lv>
2022-03-23 19:26:39 +0200

    Day 14 - Task 1

--- SGTCalculator/SGTCalculator.cs
+++ SGTCalculator/SGTCalculator.cs
@@ -1,143 +1,77 @@
-﻿namespace SGTCalculator // Note: actual namespace depends on the project name.
+﻿
+using System.Text.RegularExpressions;
+
+namespace SGTCalculator 
 {
     public class SGTCalculator
     {
         static void Main(string[] args)
         {
-            // Define variables
-            int varA, varB, varC;
-            double varX, varY, varZ;
-            double result;
+            string[] cities = { "Copenhagen", "Paris", "Tokyo", "Bristol",
+                "Mumbai", "Delhi", "Riga", "Vienna", "Warsaw", "Hamburg", "Cesis" };
+            int[] cityLength = new int[cities.Length];
 
-            // Variant 1. Valerijs D.
-            // a^3 + 5*b + sqrt(x)
-            // a = 7, b = 95, x = 179.62
-            varA = 7;
-            varB = 95;
-            varX = 179.62;
+            Regex re = new Regex("^C", RegexOptions.IgnoreCase);
+            int cCount = 0;
+            Regex re2 = new Regex("i$", RegexOptions.IgnoreCase);
+            int iCount = 0;
+            int count5Letters = 0;
+            Regex re3 = new Regex("[*e]", RegexOptions.IgnoreCase);
+            int eCount = 0;
+            Regex re4 = new Regex("(en)");
+            int enCount = 0;
 
-            result = Math.Pow(varA, 3) + 5 * varB + Math.Sqrt(varX);
-            Console.WriteLine("Variant 1. Valerijs D.");
-            Console.WriteLine(result);
-          
-            // Next Variants should goc below this line
-
-            // Variant 4. Lorianda
-            // 5a - 3b + c^7 - x | a=6, b=4, c=2, x=1.15
-            varA = 6;
-            varB = 4;
-            varC = 2;
-            varX = 1.15;
-
-            result = 5 * varA - 3 * varB + Math.Pow(varC, 7) - varX;
-            Console.WriteLine("Variant 4. Lorianda.");
-            Console.WriteLine(result);
-            
-          
-            // Next Variants should goc below this line
-            
-            // Variant 5. Greta K.
-            // 9a + sqrt(2b) - c | a = 7, b = 26, c = 16
-
-            varA = 7;
-            varB = 26;
-            varC = 16;
-            result = 9 * varA + Math.Sqrt(2 * varB) - varC;
-            Console.WriteLine($"Variant 5. Greta K. \n{result}");
-
-            // Next Variants should goc below this line
-
-            // Variant 11. Ieva P.
-            // 7a + 12.5b + x +5y | a=5, b=1, x=7.01, y=3
-
-            varA = 5;
-            varB = 1;
-            varX = 7.01;
-            varY = 3;
-
-            result = 7 * varA + 12.5 * varB + varX + 5 * varY;
-            Console.WriteLine("Variant 11.Ieva P.");
-            Console.Write(result);
-         
-            // Next Variants should go below this line
-            
-            // Variant 16. Olga S.
-            // 5a - 3b + c ^ 7 - x
-            // a = 6, b = 4, c = 2, x = 1.15
-        
-            varA = 6;
-            varB = 4;
-            varC = 2;
-            varX = 1.15;
-
-            Console.WriteLine();
-            Console.WriteLine("Variant 16. Olga S.");
-            Console.WriteLine(result);
-                   
-            // Next Variants should go below this line
-            
-            // Variant 17. Indre
-            // 9a + sqrt(2b) - c | a = 7, b = 26, c = 16
-
-            varA = 7;
-            varB = 26;
-            varC = 16;
-
-            result = 9 * varA + Math.Sqrt(2 * varB) - varC;
-            Console.WriteLine("Variant17. Indre");
-            Console.WriteLine(result);
-         
-            // Next Variants should go below this line
-            
-            // Variant 21. Valerijs D.
-            // 9a + sqrt(2b) -c | a=7, b=26, c=16 
-
-            varA = 7;
-            varB = 26;
-            varC = 16;
-
-            result = 9 * varA + Math.Sqrt(2 * varB) - varC;
-            Console.WriteLine("Variant 21. Valerijs D.");
-            Console.WriteLine(result);
-            
-            // Next Variants should goc below this line
-            // Variant 9. Julija N.
-            // 9a + sqrt(2b) -c | a=7, b=26, c=16
-
-            varA = 7;
-            varB = 26;
-            varC = 16;
-
-            result = 9 * varA + Math.Sqrt(2 * varB) - varC;
-            Console.WriteLine("Variant 9. Julija N.");
-            Console.WriteLine(result);
-
-                   
-            // Next Variants should go below this line
-            
-            //22.Extra if wanted Zane
-            //2a + 3 * b - x ^ 2 | a = 7, b = 2, x = 1.55
-
-            varA = 7;
-            varB = 2;
-            varX = 1.55;
-            result = 2 * varA + 3 * varB - Math.Pow(varX, 2);
-
-            Console.WriteLine("Variant 22. Zane");
-            Console.WriteLine(result);
-          
-          //Variant 14. Irena 
-          // 2a + 3b - x ^ 2 | a = 7, b = 2, x = 1.55
-            varA = 7;
-            varB = 2;
-            varX = 1.55;
-            result = 2 * varA + 3 * varB -Math.Pow(varX, 2);
-            Console.WriteLine("Variant 14. Irena");
-            Console.WriteLine(result);
+            for (int i = 0; i < cities.Length; i++)
+            {
+                if (re.IsMatch(cities[i]))
+                {
+                    cCount++;
+                    Console.WriteLine($"Starts with C: {cities[i]}");
+                }
+                /*
+                 if(cities[i][cities[i].Length-1] == 'i')
+                 {
+                    Console.WriteLine(cities[i]);
+                 }
+                 */
+                if (re2.IsMatch(cities[i]))
+                {
+                    iCount++;
+                    Console.WriteLine($"Ends with i: {cities[i]}");
+                }
+                if (cities[i].Length == 5)
+                {
+                    count5Letters++;
+                    Console.WriteLine($"Length is 5: {cities[i]}");
+                }
+                if (re3.IsMatch(cities[i]))
+                {
+                    eCount++;
+                    Console.WriteLine($"Contains e: {cities[i]}");
+                }
+                /*if (cities[i].IndexOf('e') != -1)
+                {
+                    eCount++;
+                    Console.WriteLine($"Contains e: {cities[i]}");
+                }*/
+                if (re4.IsMatch(cities[i]))
+                {
+                    enCount++;
+                    Console.WriteLine($"Contains en: {cities[i]}");
+                }
+                cityLength[i] = cities[i].Length;
+            }
+            Console.WriteLine($"City count starting with letter \"C\": {cCount}");
+            Console.WriteLine($"City count ending with letter \"i\": {iCount}");
+            Console.WriteLine($"City count with length 5 : {count5Letters}");
+            Console.WriteLine($"City count contains \"e\" : {eCount}");
+            Console.WriteLine($"City count contains \"en\" : {enCount}");
 
 
-            Console.ReadKey();
-
+            for (int i = 0; i < cities.Length; i++)
+            {
+                Console.WriteLine($"{cities[i]} = {cityLength[i]}");
+            }
         }
     }
 }
