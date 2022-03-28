82ca3e841a8ed7cce67a505fbb5352091ee4367a
ValerijsDiks <valerijs.diks@lightsoft.lv>
2022-03-28 21:01:26 +0300

    Dog example

---
+++ SGTCalculator/Dog.cs
@@ -0,0 +1,70 @@
+﻿namespace SGTCalculator
+{
+    public class Dog
+    {
+        public Dog()
+        {
+            Name = "default";
+            Age = 1;
+            Height = 10;
+            Breed = "not defined";
+            Color = "Black";
+        }
+
+        public Dog(string name, int age, float Height,
+            string Lifespan, string Breed, int weight, string Color,
+            bool HadRabbiesVaccine, char Gender)
+        {
+            Name = name;
+            Age = age;
+            this.Height = Height;
+            this.Lifespan = Lifespan;
+            this.Breed = Breed;
+            Weight = weight;
+            this.Color = Color;
+            this.HadRabbiesVaccine = HadRabbiesVaccine;
+            this.Gender = Gender;
+        }
+
+        private string Name { get; set; }
+
+        private int Age { get; set; }
+
+        private float Height { get; set; }
+
+        private string Lifespan { get; set; }
+
+        private string Breed { get; set; }
+
+        private int Weight { get; set; }
+
+        private string Color { get; set; }
+
+        private bool HadRabbiesVaccine { get; set; }
+
+        private char Gender { get; set; }
+
+
+        public string GetName()
+        {
+            return Name;
+        }
+
+        public void SetName(string name)
+        {
+            if (!string.IsNullOrEmpty(name))
+            {
+                Name = name;
+            }
+            else
+            {
+                Console.WriteLine("Name is not supposed to be empty");
+            }
+        }
+
+        public void Eat()
+        {
+            Console.WriteLine($"{Name}, is chewing happily om-nom-nom");
+        }
+    }
+}
--- SGTCalculator/SGTCalculator.cs
+++ SGTCalculator/SGTCalculator.cs
@@ -5,14 +5,24 @@
     {
         static void Main(string[] args)
         {
-            DataTypes dataTypes = new DataTypes();
-            //dataTypes.Demo();
-            MathRevision math = new MathRevision();
-            //math.Demo();
-            Branching branching = new Branching();
-            //branching.Demo();
-            ArraysAndLoops arraysAndLoops = new ArraysAndLoops();
-            arraysAndLoops.Demo();
+            Dog dog1 = new Dog("Bobby",3,10,"12-16", "American Staffordshire Terrier", 25,"Brown", true, 'M');
+
+            dog1.SetName("Bobby");
+            Dog dog2 = new Dog("Snowflake", 5, 20, "10-12", "Caucasian Shepherd Dog", 120, "Brown",true, 'F');
+
+            dog2.SetName("Snowflake");
+
+            Console.WriteLine($"{dog1.GetName()}, {dog2.GetName()}");
+
+            dog1.Eat();
+
+            dog2.Eat();
+            
+            Dog noArgumentConsturctorDog = new Dog();
+
+            Console.WriteLine($"{noArgumentConsturctorDog.GetName()}");
+            noArgumentConsturctorDog.Eat();
+
         }
     }
 }
